67cf3f03b4dd4ea7cc4a9ecec0f4d0e7d6382ad0
katryo <katoryo55@gmail.com>
2018-04-04 17:25:14 -0700

    Update a little

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Button from "material-ui/Button";
+import CssBaseline from "material-ui/CssBaseline";
+import Input from "material-ui/Input";
+import Icon from "material-ui/Icon";
+import TextField from "material-ui/TextField";
+import Grid from "material-ui/Grid";
+import { FormControl, FormHelperText } from "material-ui/Form";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
 
 class SearchForm extends React.Component {
   constructor(props) {
@@ -21,12 +29,23 @@ class SearchForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input
-          type="test"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <input type="submit" value="Search" />
+        <Grid container spacing={12}>
+          <Grid item sm={10} xs={8}>
+            <TextField
+              id="search"
+              label="What word do you want to learn?"
+              placeholder="Example: "
+              value={this.state.value}
+              onChange={this.handleChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="primary" size="large" onClick={this.handleSubmit}>
+              <Icon>search</Icon>Search
+            </Button>
+          </Grid>
+        </Grid>
       </form>
     );
   }
@@ -35,13 +54,18 @@ class SearchForm extends React.Component {
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Search and Learn English</h1>
-        </header>
-        <p>Search a word you want to learn.</p>
-        <SearchForm />
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div>
+          <Grid container justify="center" spacing={12}>
+            <Grid item sm={10} xs={11}>
+              <h1>Search and Learn English</h1>
+              <p>Search a word you want to learn.</p>
+              <SearchForm />
+            </Grid>
+          </Grid>
+        </div>
+      </React.Fragment>
     );
   }
 }
